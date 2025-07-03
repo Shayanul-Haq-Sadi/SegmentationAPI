f550487f6b1b445c733ecd32cea21dec3039b9b9
Shayanul-Haq-Sadi <shayanul@braincraftapps.com>
2025-07-03 10:18:15 +0600

    Initial Commit

---
+++ SegmentationAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentationAPI
+//
+//  Created by SHAYAN's Dey 15 on 7/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
